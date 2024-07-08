b49ff7e0ade85326b03dbd59543395a1d0e30739
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2024-07-08 12:04:52 +0300

    Start float filters

--- src/rendering/renderers/shared/texture/TexturePool.ts
+++ src/rendering/renderers/shared/texture/TexturePool.ts
@@ -1,4 +1,4 @@
-import { nextPow2 } from '../../../../maths/misc/pow2';
+import { nextPow2, type Size } from '../../../../maths/index.js';
 import { TextureSource } from './sources/TextureSource';
 import { Texture } from './Texture';
 
@@ -10,6 +10,7 @@ let count = 0;
  * Texture pool, used by FilterSystem and plugins.
  *
  * Stores collection of temporary pow2 or screen-sized renderTextures
+ * Can use screen-size adjustments in case of big textures
  *
  * If you use custom RenderTexturePool for your filters, you can use methods
  * `getFilterTexture` and `returnFilterTexture` same as in default pool
@@ -32,6 +33,29 @@ export class TexturePoolClass
     private _texturePool: {[x in string | number]: Texture[]};
     private _poolKeyHash: Record<number, number> = Object.create(null);
 
+    // Those are screen-related options
+    private _screenWidth = 0;
+    private _screenHeight = 0;
+    private _pixelsWidth = 0;
+    private _pixelsHeight = 0;
+    /**
+     * all textures bigger than that will have screen-pow2 size
+     */
+    screenThreshold = 512;
+    /**
+     * screen-size will be aligned on this value
+     * @private
+     */
+    screenAlign = 1;
+    /**
+     * padding for screen, one-sided, do * 2 if you need from both
+     */
+    screenPadding = 0;
+    /**
+     * scaling factor for screen-based thingy
+     */
+    screenSizeFactor = 1.4;
+
     /**
      * @param textureOptions - options that will be passed to BaseRenderTexture constructor
      * @param {SCALE_MODE} [textureOptions.scaleMode] - See {@link SCALE_MODE} for possible values.
@@ -48,8 +72,9 @@ export class TexturePoolClass
      * @param pixelWidth - Width of texture in pixels.
      * @param pixelHeight - Height of texture in pixels.
      * @param antialias
+     * @param hdr
      */
-    public createTexture(pixelWidth: number, pixelHeight: number, antialias: boolean): Texture
+    public createTexture(pixelWidth: number, pixelHeight: number, antialias: boolean, hdr: boolean): Texture
     {
         const textureSource = new TextureSource({
             ...this.textureOptions,
@@ -58,6 +83,7 @@ export class TexturePoolClass
             height: pixelHeight,
             resolution: 1,
             antialias,
+            format: hdr ? 'rgba16float' : undefined,
             autoGarbageCollect: true,
         });
 
@@ -73,17 +99,44 @@ export class TexturePoolClass
      * @param frameHeight - The minimum height of the render texture.
      * @param resolution - The resolution of the render texture.
      * @param antialias
+     * @param hdr
      * @returns The new render texture.
      */
-    public getOptimalTexture(frameWidth: number, frameHeight: number, resolution = 1, antialias: boolean): Texture
+    public getOptimalTexture(frameWidth: number, frameHeight: number, resolution = 1, antialias: boolean, hdr: boolean): Texture
     {
-        let po2Width = Math.ceil((frameWidth * resolution) - 1e-6);
-        let po2Height = Math.ceil((frameHeight * resolution) - 1e-6);
+        let width = Math.ceil((frameWidth * resolution) - 1e-6);
+        let height = Math.ceil((frameHeight * resolution) - 1e-6);
 
-        po2Width = nextPow2(po2Width);
-        po2Height = nextPow2(po2Height);
+        let screenWidth = this._pixelsWidth;
+        let screenHeight = this._pixelsHeight;
 
-        const key = (po2Width << 17) + (po2Height << 1) + (antialias ? 1 : 0);
+        let sign: number;
+
+        if (width < this.screenThreshold || height < this.screenThreshold
+            || width > screenWidth || height > screenHeight)
+        {
+            width = nextPow2(width);
+            height = nextPow2(height);
+            sign = 1;
+        }
+        else
+        {
+            const factor = this.screenSizeFactor;
+
+            while (width <= Math.ceil(screenWidth / factor))
+            {
+                screenWidth = Math.ceil(screenWidth / factor);
+            }
+            while (height <= Math.ceil(screenHeight / factor))
+            {
+                screenHeight = Math.ceil(screenHeight / factor);
+            }
+            width = screenWidth;
+            height = screenHeight;
+            sign = -1;
+        }
+
+        const key = sign * ((width << 16) + (height << 2) + (antialias ? 1 : 0) + (hdr ? 2 : 0));
 
         if (!this._texturePool[key])
         {
@@ -94,14 +147,14 @@ export class TexturePoolClass
 
         if (!texture)
         {
-            texture = this.createTexture(po2Width, po2Height, antialias);
+            texture = this.createTexture(width, height, antialias, hdr);
         }
 
         texture.source._resolution = resolution;
-        texture.source.width = po2Width / resolution;
-        texture.source.height = po2Height / resolution;
-        texture.source.pixelWidth = po2Width;
-        texture.source.pixelHeight = po2Height;
+        texture.source.width = width / resolution;
+        texture.source.height = height / resolution;
+        texture.source.pixelWidth = width;
+        texture.source.pixelHeight = height;
 
         // fit the layout to the requested original size
         texture.frame.x = 0;
@@ -122,11 +175,11 @@ export class TexturePoolClass
      * @param antialias - Whether to use antialias.
      * @returns A texture that is a power of two
      */
-    public getSameSizeTexture(texture: Texture, antialias = false)
+    public getSameSizeTexture(texture: Texture, antialias = false, hdr = false)
     {
         const source = texture.source;
 
-        return this.getOptimalTexture(texture.width, texture.height, source._resolution, antialias);
+        return this.getOptimalTexture(texture.width, texture.height, source._resolution, antialias, hdr);
     }
 
     /**
@@ -164,6 +217,50 @@ export class TexturePoolClass
         }
 
         this._texturePool = {};
+    }
+
+    /**
+     * If screen size was changed, drops all screen-sized textures,
+     * sets new screen size, sets `enableFullScreen` to true
+     *
+     * Size is measured in pixels, `renderer.view` can be passed here, not `renderer.screen`
+     * @param size - Initial size of screen.
+     */
+    setScreenSize(size: Size): void
+    {
+        if (size.width === this._screenWidth
+            && size.height === this._screenHeight)
+        {
+            return;
+        }
+
+        if (this._pixelsWidth > 0)
+        {
+            for (const i in this._texturePool)
+            {
+                if (!(Number(i) < 0))
+                {
+                    continue;
+                }
+                const textures = this._texturePool[i];
+
+                if (textures)
+                {
+                    for (let j = 0; j < textures.length; j++)
+                    {
+                        textures[j].destroy(true);
+                    }
+                }
+            }
+        }
+
+        this._screenWidth = size.width;
+        this._screenHeight = size.height;
+
+        const { screenAlign, screenPadding } = this;
+
+        this._pixelsWidth = Math.ceil((size.width + screenPadding + screenAlign) / screenAlign) * screenAlign;
+        this._pixelsHeight = Math.ceil((size.height + screenPadding + screenAlign) / screenAlign) * screenAlign;
     }
 }
 

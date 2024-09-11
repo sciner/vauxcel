2ad9fa5953de43dbf81683c346300af88f48b478
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2024-09-11 19:10:47 +0300

    Fix batcher texture check

--- src/rendering/batcher/shared/Batcher.ts
+++ src/rendering/batcher/shared/Batcher.ts
@@ -290,6 +290,8 @@ export class Batcher
                 continue;
             }
 
+            // TODO: check again in execute?
+            source.checkUpdate();
             source._batchTick = BATCH_TICK;
 
             if (textureBatch.count >= maxTextures || blendModeChange)
